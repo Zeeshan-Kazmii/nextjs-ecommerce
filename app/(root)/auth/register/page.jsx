63b90d0b46616bdf6c0d7e211d90c925961c8c6b
Zeeshan Kazmi <sz7185511@gmail.com>
2026-04-01 13:27:38 +0500

    register UI

--- app/(root)/auth/register/page.jsx
+++ app/(root)/auth/register/page.jsx
@@ -17,47 +17,28 @@ import ButtonLoading from '@/components/Application/ButtonLoading'
 import {z} from 'zod'
 import { FaRegEyeSlash,FaRegEye } from 'react-icons/fa'
 import Link from 'next/link'
-import { WEBSITE_REGISTER } from '@/routes/WebsiteRoute'
-const LoginPage = () => {
-    const formSchema = zSchema.pick({ email: true }).extend({password: z.string().min(3, 'Password is required')})
+import { WEBSITE_LOGIN } from '@/routes/WebsiteRoute'
+const RegisterPage = () => {
+    const formSchema = zSchema.pick({ email: true, password: true }).extend({
+        confirmPassword: z.string()})
+        .refine((data) => data.password === data.confirmPassword, {
+            message: "Passwords do not match",
+            path: ["confirmPassword"],
+        });
     const form = useForm({
     resolver: zodResolver(formSchema),
     defaultValues: {
       email: "",
       password: "",
+      confirmPassword: "",
     },
   })
 
   const [loading, setLoading] = useState(false)
   const [isTypePassword, setIsTypePassword] = useState(true)
 
-const handleLoginSubmit = async (values) => {
-//   try {
-//     setLoading(true)
-
-    // console.log("Form values:", values)
-
-//     const res = await fetch("/api/login", {
-//       method: "POST",
-//       headers: {
-//         "Content-Type": "application/json",
-//       },
-//       body: JSON.stringify(values),
-//     })
-
-//     const data = await res.json()
-
-//     if (!res.ok) {
-//       throw new Error(data.message || "Login failed")
-//     }
-
-//     console.log("Login success:", data)
-
-//   } catch (error) {
-//     console.error(error.message)
-//   } finally {
-//     setLoading(false)
-//   }
+const handleRegisterSubmit = async (values) => {
+    console.log(values)
 }
 
   return (
@@ -67,13 +48,28 @@ const handleLoginSubmit = async (values) => {
 <Image src={Logo} alt="logo" className="max-w-[150px]" />
       </div>
             <div className='text-center'>
-                <h1 className='text-3xl font-bold'>Login Into Account</h1>
-                <p>Login into your account by filling out the form below.</p>
+                <h1 className='text-3xl font-bold'>Create Account</h1>
+                <p>Create a new account by filling out the form below.</p>
             </div>
             <div className='mt-5'>
-                <form onSubmit={form.handleSubmit(handleLoginSubmit)}>
+                <form onSubmit={form.handleSubmit(handleRegisterSubmit)}>
         <FieldGroup>
           <FieldSet>
+    <Controller
+    name="name"
+  control={form.control}
+  render={({ field, fieldState }) => (
+    <Field>
+      <FieldLabel htmlFor="name">Full Name</FieldLabel>
+      <Input
+        type="text"
+        placeholder="John Doe"
+        id="name"
+        {...field}
+      />
+    </Field>
+  )}
+/>
     <Controller
     name="email"
   control={form.control}
@@ -101,7 +97,23 @@ const handleLoginSubmit = async (values) => {
     <Field >
       <FieldLabel htmlFor="password">Password</FieldLabel>
       <div className='relative'>
-    <Input type={isTypePassword ? "password" : "text"} placeholder='Enter your password' id="password" {...field} />
+    <Input type="password" placeholder='Enter your password' id="password" {...field} />
+    
+    </div>
+      {fieldState.error && (
+        <p className="text-red-500 text-xs mt-1">{fieldState.error.message}</p>
+      )}
+    </Field>
+  )}
+/>
+            <Controller
+  name="confirmPassword"
+  control={form.control}
+  render={({ field, fieldState }) => (
+    <Field >
+      <FieldLabel htmlFor="confirmPassword">Confirm Password</FieldLabel>
+      <div className='relative'>
+    <Input type={isTypePassword ? "password" : "text"} placeholder='Enter your password' id="confirmPassword" {...field} />
     <button type='button' className='absolute right-3 top-1/2 -translate-y-1/2 cursor-pointer' onClick={() => setIsTypePassword(!isTypePassword)}>
       {isTypePassword ? <FaRegEyeSlash /> : <FaRegEye />}
     </button>
@@ -116,18 +128,15 @@ const handleLoginSubmit = async (values) => {
         </FieldGroup>
         <ButtonLoading 
   type="submit" 
-  text="Login"
+  text="Create Account"
     loading={loading}
 //   onClick={form.handleSubmit(handleLoginSubmit)}
   className="w-full mt-4 cursor-pointer"
 />
 <div className='text-center'>
   <div className='flex justify-center items-center gap-2 mt-2'>
-  <p>Don't have an account?</p>
-  <Link href={WEBSITE_REGISTER} className="text-primary underline">Register here</Link>
-</div>
-<div className='mt-2'>
-  <Link href="" className="text-primary underline">Forgot password?</Link>
+  <p>Already have an account?</p>
+  <Link href={WEBSITE_LOGIN} className="text-primary underline">Login here</Link>
 </div>
 </div>
       </form>
@@ -137,4 +146,5 @@ const handleLoginSubmit = async (values) => {
   )
 }
 
-export default LoginPage
+
+export default RegisterPage
