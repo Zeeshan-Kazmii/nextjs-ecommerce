c55a6d071330038e1c1096a164543cd792b76496
Zeeshan Kazmi <sz7185511@gmail.com>
2026-04-18 19:28:21 +0500

    admin login credentials setup

---
+++ app/(root)/(admin)/admin/dashboard/page.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+
+const AdminDashboard = () => {
+  return (
+    <div>Admin Dashboard {console.log('Admin Dashboard')}</div>
+    
+  )
+}
+
+export default AdminDashboard
--- app/(root)/auth/login/page.jsx
+++ app/(root)/auth/login/page.jsx
@@ -17,14 +17,18 @@ import ButtonLoading from '@/components/Application/ButtonLoading'
 import {z} from 'zod'
 import { FaRegEyeSlash,FaRegEye } from 'react-icons/fa'
 import Link from 'next/link'
-import { WEBSITE_REGISTER, WEBSITE_RESETPASSWORD } from '@/routes/WebsiteRoute'
+import { USER_DASHBOARD, WEBSITE_REGISTER, WEBSITE_RESETPASSWORD } from '@/routes/WebsiteRoute'
 import axios from 'axios'
 import { showToast } from '@/lib/showTost'
 import OTPVerification from '@/components/Application/OTPVerification'
 import { useDispatch } from 'react-redux'
 import { login } from '@/store/reducer/authReducer'
+import { useRouter, useSearchParams } from 'next/navigation'
+import { ADMIN_DASHBOARD } from '@/routes/AdminPanelRoute'
 const LoginPage = () => {
   const dispatch = useDispatch() 
+  const searchParams = useSearchParams()
+  const router = useRouter()
   const formSchema = zSchema.pick({ email: true }).extend({password: z.string().min(3, 'Password is required')})
   const form = useForm({
     resolver: zodResolver(formSchema),
@@ -68,6 +72,11 @@ const handleotpVerification = async (values)=>{
       showToast('success', otpResponse.message)
 
       dispatch(login(otpResponse.data))
+      if(searchParams.has('callback')) {
+        router.push(searchParams.get('callback'))
+      } else {
+        otpResponse.data.role === 'admin' ? router.push(ADMIN_DASHBOARD) : router.push(USER_DASHBOARD)
+      }
 
     } catch (error) {
       showToast('error', error.message)
